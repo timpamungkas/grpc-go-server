fee2561b6f586aa725f280fb7532ca9c1e851d71
timpamungkas <83567132+timpamungkas-bfi@users.noreply.github.com>
2023-03-08 06:08:02 +0700

    fix migration

--- internal/adapter/grpc/bank_adapter.go
+++ internal/adapter/grpc/bank_adapter.go
@@ -136,6 +136,21 @@ func toTime(dt *datetime.DateTime) (time.Time, error) {
 	return res, nil
 }
 
+func currentDatetime() *datetime.DateTime {
+	now := time.Now()
+
+	return &datetime.DateTime{
+		Year:       int32(now.Year()),
+		Month:      int32(now.Month()),
+		Day:        int32(now.Day()),
+		Hours:      int32(now.Hour()),
+		Minutes:    int32(now.Minute()),
+		Seconds:    int32(now.Second()),
+		Nanos:      int32(now.Second()),
+		TimeOffset: &datetime.DateTime_UtcOffset{},
+	}
+}
+
 func (a *GrpcAdapter) TransferMultiple(stream bank.BankService_TransferMultipleServer) error {
 	for {
 		req, err := stream.Recv()
@@ -164,6 +179,7 @@ func (a *GrpcAdapter) TransferMultiple(stream bank.BankService_TransferMultipleS
 		res := bank.TransferResponse{
 			FromAccountNumber: req.FromAccountNumber,
 			ToAccountNumber:   req.ToAccountNumber,
+			Currency:          req.Currency,
 			Amount:            req.Amount,
 			Timestamp:         currentDatetime(),
 		}
@@ -181,19 +197,4 @@ func (a *GrpcAdapter) TransferMultiple(stream bank.BankService_TransferMultipleS
 		}
 	}
 
-}
-
-func currentDatetime() *datetime.DateTime {
-	now := time.Now()
-
-	return &datetime.DateTime{
-		Year:       int32(now.Year()),
-		Month:      int32(now.Month()),
-		Day:        int32(now.Day()),
-		Hours:      int32(now.Hour()),
-		Minutes:    int32(now.Minute()),
-		Seconds:    int32(now.Second()),
-		Nanos:      int32(now.Second()),
-		TimeOffset: &datetime.DateTime_UtcOffset{},
-	}
 }
